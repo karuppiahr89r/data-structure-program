b03511783872cc0a049d952da21b689a5447957d
KARUPPIAH R <karuppiahr89@gmail.com>
2026-01-30 19:23:25 +0530

    Format and clean up pop.c code

--- pop.c
+++ pop.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#define MAX 5   // Maximum size of stack
-
+#define MAX 5 
 int stack[MAX];
 int top = -1;
 
-// Function to pop an element from stack
-void pop() {
-    if (top == -1) {
+
+void pop()
+{
+    if (top == -1) 
+    {
         printf("Stack Underflow! No element to pop\n");
     } else {
         printf("Popped element: %d\n", stack[top]);
@@ -14,15 +15,18 @@ void pop() {
     }
 }
 
-int main() {
+int main()
+{
     int n, value;
 
     printf("Enter number of elements to push initially: ");
     scanf("%d", &n);
 
-    // Push elements initially (for pop operation)
-    for (int i = 0; i < n; i++) {
-        if (top == MAX - 1) {
+   
+    for (int i = 0; i < n; i++) 
+    {
+        if (top == MAX - 1)
+        {
             printf("Stack Overflow!\n");
             break;
         }
@@ -32,8 +36,9 @@ int main() {
         stack[top] = value;
     }
 
-    // Perform pop operation
+  
     pop();
 
     return 0;
 }
+
