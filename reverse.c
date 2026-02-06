a14bfcf7919cb21bce36dd4735bf4962c6ab911b
KARUPPIAH R <karuppiahr89@gmail.com>
2026-02-06 13:43:18 +0530

    Fix formatting issues in reverse.c

--- reverse.c
+++ reverse.c
@@ -24,13 +24,11 @@ int main()
     printf("Enter a string: ");
     scanf("%s", str);
 
-    // Push all characters into stack
     for(i = 0; str[i] != '\0'; i++)
         push(str[i]);
-
-    // Pop characters to reverse string
     for(i = 0; top != -1; i++)
         str[i] = pop();
     printf("Reversed string = %s\n", str);
     return 0;
 }
+
