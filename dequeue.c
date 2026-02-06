65ff81970c4abc754e4998f06b74e325ded025d9
KARUPPIAH R <karuppiahr89@gmail.com>
2026-02-06 13:36:02 +0530

    Add files via upload

---
+++ dequeue.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#define MAX 5
+
+int queue[MAX] = {10,20,30,40,50};
+int front = 0;
+int rear = 4;
+
+// Dequeue Operation
+void dequeue()
+{
+    if(front > rear)
+        printf("Queue Underflow\n");
+    else
+    {
+        printf("Deleted element = %d\n", queue[front]);
+        front++;
+    }
+}
+
+int main()
+{
+    dequeue();
+    dequeue();
+    return 0;
+}
